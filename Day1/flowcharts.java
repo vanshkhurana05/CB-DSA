4701cbffa114ba3f181ae42c101800827cc1230c
vanshkhurana05 <5vanshkhurana@gmail.com>
2024-07-14 00:30:11 +0530

    first class

---
+++ Day1/flowcharts.java
@@ -0,0 +1,47 @@
+package Day1;
+import java.util.Scanner;
+public class flowcharts{
+    public static void main(String[] args) {
+        //input
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        
+       //output
+        System.out.println(a);
+        sc.close();
+       
+       
+        //int:4 bytes
+        //float:4bytes
+        //short:2bytes
+        //bytes:1byte
+        //double:8bytes decimal 15 decimals
+        //char:2bytes
+        //long:8bytes
+        //boolean:1byte
+
+
+
+        //operators
+        //1.arithematic
+        //2.logical:&&(and) , ||(or),!not
+        //3.comparison: x>y(greater then) , x!=y(not equal)  ,x==y(eqaut to), x>=y ,x<=y
+        //4.bitwise: <<, >> , &, |,^,~
+        //5.assigment operator: x=y,   x+=y::x=x+y,   -=,*=,/=,%=,
+        
+        
+        //for,while,do-while, for each
+
+        /*int num=10;
+        do{
+            num++;
+        }while(num<15);*/
+
+        //git command lines (most common)
+        //git init (can be skipped if already done once)
+        // git add .
+        // git commit -m "message to be written"
+        // git push
+    }
+
+}
