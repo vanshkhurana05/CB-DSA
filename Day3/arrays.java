9afbb30d5d2037fd89540bf87f208eee7ef63903
vanshkhurana05 <5vanshkhurana@gmail.com>
2024-07-20 12:29:06 +0530

    Third class

---
+++ Day3/arrays.java
@@ -0,0 +1,15 @@
+package Day3;
+
+public class arrays {
+    static void printArr(int[] arr){
+        //for each loop
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] arr=new int[10];
+        System.out.println(arr);
+    }
+}
